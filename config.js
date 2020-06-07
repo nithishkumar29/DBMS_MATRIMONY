9785b7f011aca931eddabc259c4a7227cb6a523e
sirbrucewayne <rangavamsi5@gmail.com>
2020-06-07 22:55:25 +0530

    config fix

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
 	connection : mysql.createConnection ({
 		host : 'localhost',
 		database : 'matrimony',//'DBMSproject',
-		user : 'root', // mysql username
-	 	password : 'vamsi123', //mysql password
+		user : '', // mysql username
+	 	password : '', //mysql password
 	})
 }
